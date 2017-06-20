f11a5d37f5a7e8a9851ad1aaf4a3db494533f029
github username <github email address>
2017-06-20 18:27:51 -0400

    updated ish

--- bin/run.rb
+++ bin/run.rb
@@ -3,12 +3,11 @@ require 'pry'
 require_relative '../lib/data.rb'
 
 
-db = SQLite3::Database.new('../db/bob_ross.db')
-sql_runner = SQLRunner.new(db)
+DB = SQLite3::Database.new('./db/bob_ross.db')
+sql_runner = SQLRunner.new(DB)
 
-sql_runner.execute_insert_arg(Data.insert_1)
-sql_runner.execute_insert_arg(Data.insert_2)
+# sql_runner.execute_create_sql
+# sql_runner.execute_insert_arg(Data.insert_1)
+# sql_runner.execute_insert_arg(Data.insert_2)
 sql_runner.execute_insert_arg(Data.insert_3)
-sql_runner.execute_insert_arg(Data.insert_4)
-
-Pry.start
+# sql_runner.execute_insert_arg(Data.insert_4)
--- lib/data.rb
+++ lib/data.rb
@@ -7,19 +7,19 @@ attr_accessor
 @@extracted_data = CSV.table('bin/elements-by-episode.csv')
 @@transform = @@extracted_data.map { |row| row.to_h }
 
-values_1 = transform.each_with_object([]) { |row, arr|  arr << "(#{row[:episode].slice(1, 2)})"}.uniq.join(", ")
+values_1 = @@transform.each_with_object([]) { |row, arr|  arr << "(#{row[:episode].slice(1, 2)})"}.uniq.join(", ")
 
 @@insert_1 = "INSERT INTO seasons (season) VALUES #{values_1};"
 
-values_2 = transform.each_with_object([]) { |row, arr| arr << "(#{row[:episode].split("E").pop}, #{row[:title]}, #{row[:episode].slice(1, 2)})" }.join(", ")
+values_2 = @@transform.each_with_object([]) { |row, arr| arr << "(#{row[:episode].split("E").pop}, #{row[:title]}, #{row[:episode].slice(1, 2)})" }.join(", ")
 
-@@insert_2 = "INSERT INTO episodes (episode, title, season_id) VALUES #{values_2};"
+@@insert_2 = "INSERT INTO episodes (episode_number, title, season_id) VALUES #{values_2};"
 
-values_3 = transform[0].keys.pop(transform[0].keys.size - 2).map {|element| "(#{element})"}.join(", ")
+values_3 = @@transform[0].keys.pop(@@transform[0].keys.size - 2).map {|element| "(#{element})"}.join(", ")
 
-@@insert_3 = "INSERT INTO elements (element) VALUES #{values_3}"
+@@insert_3 = "INSERT INTO elements (element) VALUES #{values_3};"
 
-values_4 = transform.each_with_object([]) do |row, arr|
+values_4 = @@transform.each_with_object([]) do |row, arr|
   row.each_with_index do |(column, value), index|
     if value == 1
       arr << "(#{row[:episode].split("E").pop}, #{index - 1})"
@@ -30,21 +30,21 @@ end.join(', ')
 
 @@insert_4 = "INSERT INTO episodes_elements (ep_id, el_id) VALUES #{values_4};"
 
-def insert_1
+def self.insert_1
   @@insert_1
 end
 
-def insert_2
+def self.insert_2
   @@insert_2
 end
 
-def insert_3
+def self.insert_3
   @@insert_3
 end
 
-def insert_4
+def self.insert_4
   @@insert_4
 end
 
-Pry.start
 end
+Pry.start
